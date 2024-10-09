21dfadd0c6530f8d538a18e014abc326e33578fa
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-09 23:46:01 +0530

    use the nextPrime function and print the Prime Pyramid Pattern

--- 00_assignments/lec9_loops/PrimePyramid.java
+++ 00_assignments/lec9_loops/PrimePyramid.java
@@ -18,20 +18,19 @@ Output2:
 
 public class PrimePyramid {
     public static void main(String[] args) {
-        // int r = 6;
-        // int col = 5;
-        // int m = 1;
-        // for (int i = 1; i <= r; i++) {
-        // for (int j = i; j <= col; j++) {
-        // System.out.print(" ");
-        // }
-        // for (int k = 1; k <= i; k++) {
-        // System.out.print(m + " ");
-        // m++;
-        // }
-        // System.out.println();
-        // }
-        System.out.println(nextPrimeNo(61));
+        int r = 6;
+        int col = 5;
+        int prime = 1;
+        for (int i = 1; i <= r; i++) {
+            for (int j = i; j <= col; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                prime = nextPrimeNo(prime);
+                System.out.print(prime + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static int nextPrimeNo(int i) {
