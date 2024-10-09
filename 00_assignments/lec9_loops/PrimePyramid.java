9296d0fce74b3725e4f6db48c5540b176a641ab4
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-09 23:22:48 +0530

    make Prime Pyramid Pattern v1.0

---
+++ 00_assignments/lec9_loops/PrimePyramid.java
@@ -0,0 +1,33 @@
+/*
+ * Q8 - Write a program to print a triangle of prime numbers upto given number of lines of the trinagle.
+ * Input1:
+2
+Output1:
+2
+3 5
+Input2:
+6
+Output2:
+2
+3 5
+7 11 13
+17 19 23 29
+31 37 41 43 47
+53 59 61 67 71 73
+ */
+
+public class PrimePyramid {
+    public static void main(String[] args) {
+        int r = 6;
+        int col = 5;
+        for (int i = 1; i <= r; i++) {
+            for (int j = i; j <= col; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
