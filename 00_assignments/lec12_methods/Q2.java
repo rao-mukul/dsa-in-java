a0b8bc3a23a4ad788e2ae6ce6b72460204b0a300
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-13 22:48:47 +0530

    solve Q4 Leap Year  Methods Assignments

---
+++ 00_assignments/lec12_methods/Q2.java
@@ -0,0 +1,13 @@
+package lec12_methods;
+
+class Vowels{
+    void vowels(String str){
+        
+    }
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 00_assignments/lec12_methods/Q4.java
@@ -0,0 +1,24 @@
+package lec12_methods;
+
+public class Q4 {
+
+    public static boolean leap(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(leap(1900));
+    }
+}
