136576919d26371e7cccda4d3ae8fb817380fd64
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-14 23:19:33 +0530

    solve Q2 count vowels in string

--- 00_assignments/lec12_methods/Q2.java
+++ 00_assignments/lec12_methods/Q2.java
@@ -1,13 +1,29 @@
 package lec12_methods;
 
-class Vowels{
-    void vowels(String str){
-        
-    }
-}
+// Q2 - Write a Java method to count all vowels in a string
 
 public class Q2 {
+
+    static int countVowels(String str) {
+        int len = str.length();
+        int count = 0;
+
+        str = str.toLowerCase(); // Convert the String to Lower Case
+
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            boolean cond1 = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+            // boolean cond2 = c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+
+            if (cond1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(countVowels("cOding"));
+        System.out.println(countVowels("heaps"));
     }
 }
