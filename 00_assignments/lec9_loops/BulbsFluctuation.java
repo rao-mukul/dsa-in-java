38a158eaab48f832182188c6e2d4f050b863f769
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-15 18:37:08 +0530

    add alternative approach for Bulb Fluctuation Q

--- 00_assignments/lec9_loops/BulbsFluctuation.java
+++ 00_assignments/lec9_loops/BulbsFluctuation.java
@@ -34,4 +34,13 @@ public class BulbsFluctuation {
             }
         }
     }
+
+    public void alternative() {
+        int n = 25;
+        for (int i = 1; i * i <= n; i++) {
+            System.out.print(i * i + " "); // only those bulbs will remain on which are perfect squares as perfect
+                                           // squares have odd number of divisors due to their square root being the
+                                           // extra one, whereas rest have even number of divisors
+        }
+    }
 }
